be23218d0f389717503778422d04d859f3785e27
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-02-07 21:14:39 -0600

    Add logic to use multiple operators together

--- javaScript.js
+++ javaScript.js
@@ -1,34 +1,74 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
+document.addEventListener('DOMContentLoaded', function() {
 
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function operate(num1, num2, operator) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-        case '-':
-            return subtract(num1, num2);
-        case 'X':
-            return multiply(num1, num2);
-        case '/':
-            return divide(num1, num2);
+    function add(num1, num2) {
+        return parseInt(num1) + parseInt(num2);
     }
-}
 
-let num1;
-let num2;
-let operator;
+    function subtract(num1, num2) {
+        return num1 - num2;
+    }
 
-console.log(operate(1, 3, 'X'));
+    function multiply(num1, num2) {
+        return num1 * num2;
+    }
+
+    function divide(num1, num2) {
+        return num1 / num2;
+    }
+
+    function operate(num1, num2, operator) {
+        switch (operator) {
+            case '+':
+                return add(num1, num2);
+            case '-':
+                return subtract(num1, num2);
+            case 'X':
+                return multiply(num1, num2);
+            case '/':
+                return divide(num1, num2);
+        }
+    }
+
+    let num1;
+    let num2;
+    let operator;
+    let result;
+    inputArr = [];
+
+    const display = document.getElementById('display');
+    const digits = document.querySelectorAll('.digit');
+    const operands = document.querySelectorAll('.operand');
+    const equalsButton = document.getElementById('equalsButton');
+
+    digits.forEach(digit => {
+        digit.addEventListener('click', () => {
+            display.innerHTML = digit.innerHTML;
+            inputArr.push(digit.innerHTML);
+        });
+    });
+
+    operands.forEach(operand => {
+        operand.addEventListener('click', () => {
+            display.innerHTML = operand.innerHTML;
+            inputArr.push(operand.innerHTML);
+        });
+    });
+    
+    equalsButton.addEventListener('click', () => {
+    inputStr = inputArr.join('');
+    inputArr = inputStr.split(/([+\-X/])/);
+    while (inputArr.length > 1) {
+        num1 = inputArr[0];
+        num2 = inputArr[2];
+        operator = inputArr[1];
+        result = operate(num1, num2, operator);
+        for (let i = 0; i < 3; i++) {
+            inputArr.shift();
+        }
+        inputArr.unshift(result);
+    }
+    console.log(result);
+    });
+    
+
+});
