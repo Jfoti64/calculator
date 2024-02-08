f542b09c5ed30b3cf3b3da027e0449d44891ebc3
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-02-07 19:48:11 -0600

    Add functions to handle operator logic

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,34 @@
-function add(num1, num2, operator)
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case 'X':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+let num1;
+let num2;
+let operator;
+
+console.log(operate(1, 3, 'X'));
